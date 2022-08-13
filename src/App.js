4f33cc7cc764acc9df529348a1ec8365d67e4aa4
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-08-13 22:29:10 +0530

    home component setup done!!!!!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+     <Routes>
+      <Route path='/home' element={<Home />} />
+     </Routes>
     </div>
   );
 }
