2fc51377f3c53b15ce239c35f00d33fe4ffccef0
Rohit Dhende <rohit.dhende@minddeft.com>
2022-08-22 21:03:04 +0530

    Removed fullWidth prop from material components as it was giving warnings

--- src/components/Modal/ContributeModal.react.jsx
+++ src/components/Modal/ContributeModal.react.jsx
@@ -48,6 +48,7 @@ ContributeModal.defaultProps = {
 //InputField styling
 const inputStyle = {
   margin: "0.5rem 0",
+  width: 1,
 };
 
 export default function ContributeModal(props) {
@@ -65,9 +66,9 @@ export default function ContributeModal(props) {
     panNumber: "",
     name: "",
     address: "",
+    state: "",
     city: "",
     zip: "",
-    state: "",
     mob: "",
     email: "",
   };
@@ -103,10 +104,12 @@ export default function ContributeModal(props) {
     }
   };
 
+  //Update City list after selecting the state
   const newCityList = cityList.filter((items) => {
     return items.state.includes(formData?.state);
   });
 
+  //Reset Data function to reset form inputs
   const resetData = () => {
     setAmount("");
     setFormData(initialFormState);
@@ -118,264 +121,243 @@ export default function ContributeModal(props) {
   }, [props.isOpen]);
 
   return (
-    <div>
-      <div>
-        <Dialog open={open}>
-          <DialogTitle>Contribute</DialogTitle>
-          <DialogContent>
-            <FormControl
-              sx={{
-                display: "flex",
-              }}
-            >
-              <RadioGroup
-                required
-                fullWidth
-                aria-labelledby="radio-buttons-group-label"
-                defaultValue={""}
-                name="radio-buttons-group"
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  flex: 1,
-                }}
-                onChange={handleAmount}
-              >
-                <FormControlLabel
-                  value="100"
-                  control={<Radio />}
-                  label="₹100"
-                />
-                <FormControlLabel
-                  value="500"
-                  control={<Radio />}
-                  label="₹500"
-                />
-                <FormControlLabel
-                  value="1000"
-                  control={<Radio />}
-                  label="₹1000"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Other Amount"
-                />
-              </RadioGroup>
-            </FormControl>
-            <FormControl fullWidth sx={inputStyle}>
-              <InputLabel htmlFor="amount-input-box" required>
-                Amount
-              </InputLabel>
-              <OutlinedInput
-                required
-                id="amount-input-box"
-                value={formData.amount === "other" ? "" : formData.amount}
-                onChange={handleForm("amount")}
-                startAdornment={
-                  <InputAdornment position="start">₹</InputAdornment>
-                }
-                label="Amount"
-                type="number"
-                disabled={amount === "other" ? false : true}
-                placeholder={amount === "" ? "Please select above options" : ""}
-              />
-              {parseInt(formData.amount) === 0 && (
-                <FormHelperText error id="accountId-error">
-                  Please enter some amount
-                </FormHelperText>
-              )}
-            </FormControl>
-            {formData.amount > 5000 && (
-              <FormControl fullWidth sx={inputStyle}>
-                <InputLabel htmlFor="pan-card-details">
-                  Enter Pan Card No (Required if donation is above ₹ 5000)
-                </InputLabel>
-                <OutlinedInput
-                  id="pan-card-details"
-                  value={formData.panNumber}
-                  onChange={handleForm("panNumber")}
-                  label="Enter Pan Card No (Required if donation is above ₹ 5000)"
-                  type="text"
-                  hidden={true}
-                  disabled={amount === "other" ? false : true}
-                  placeholder={
-                    amount === "" ? "Please select above options" : ""
-                  }
-                />
-              </FormControl>
-            )}
-            <FormControl fullWidth sx={inputStyle}>
-              <InputLabel htmlFor="name-input-box" required>
-                Name
-              </InputLabel>
-              <OutlinedInput
-                required
-                id="name-input-box"
-                label="Name"
-                onChange={handleForm("name")}
-              />
-              {/* {!isName && (
+    <Dialog open={open}>
+      <DialogTitle>Contribute</DialogTitle>
+      <DialogContent>
+        <FormControl
+          sx={{
+            display: "flex",
+          }}
+        >
+          <RadioGroup
+            required
+            aria-labelledby="radio-buttons-group-label"
+            defaultValue={""}
+            name="radio-buttons-group"
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              flex: 1,
+            }}
+            onChange={handleAmount}
+          >
+            <FormControlLabel value="100" control={<Radio />} label="₹100" />
+            <FormControlLabel value="500" control={<Radio />} label="₹500" />
+            <FormControlLabel value="1000" control={<Radio />} label="₹1000" />
+            <FormControlLabel
+              value="other"
+              control={<Radio />}
+              label="Other Amount"
+            />
+          </RadioGroup>
+        </FormControl>
+        <FormControl sx={inputStyle}>
+          <InputLabel htmlFor="amount-input-box" required>
+            Amount
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="amount-input-box"
+            value={formData.amount === "other" ? "" : formData.amount}
+            onChange={handleForm("amount")}
+            startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+            label="Amount"
+            type="number"
+            disabled={amount === "other" ? false : true}
+            placeholder={amount === "" ? "Please select above options" : ""}
+          />
+          {parseInt(formData.amount) === 0 && (
+            <FormHelperText error id="accountId-error">
+              Please enter some amount
+            </FormHelperText>
+          )}
+        </FormControl>
+        {formData.amount > 5000 && (
+          <FormControl sx={inputStyle}>
+            <InputLabel htmlFor="pan-card-details">
+              Enter Pan Card No (Required if donation is above ₹ 5000)
+            </InputLabel>
+            <OutlinedInput
+              id="pan-card-details"
+              value={formData.panNumber}
+              onChange={handleForm("panNumber")}
+              label="Enter Pan Card No (Required if donation is above ₹ 5000)"
+              type="text"
+              hidden={true}
+              disabled={amount === "other" ? false : true}
+              placeholder={amount === "" ? "Please select above options" : ""}
+            />
+          </FormControl>
+        )}
+        <FormControl sx={inputStyle}>
+          <InputLabel htmlFor="name-input-box" required>
+            Name
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="name-input-box"
+            label="Name"
+            onChange={handleForm("name")}
+          />
+          {/* {!isName && (
                 <FormHelperText error id="email-error">
                   Please enter valid name
                 </FormHelperText>
               )} */}
-            </FormControl>
-            <FormControl fullWidth sx={inputStyle}>
-              <InputLabel htmlFor="address-input-box" required>
-                Address
-              </InputLabel>
-              <OutlinedInput
-                required
-                id="address-input-box"
-                label="Address"
-                onChange={handleForm("address")}
-              />
-            </FormControl>
-            <FormControl fullWidth sx={inputStyle}>
-              <InputLabel id="state-select-label" required>
-                State
-              </InputLabel>
-              <Select
-                required
-                labelId="state-select-label"
-                id="state-select"
-                value={formData.state}
-                label="State"
-                onChange={handleForm("state")}
-              >
-                {stateList?.map((list, index) => {
-                  return (
-                    <MenuItem key={index} value={list.name}>
-                      {list.name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-            <FormGroup
-              sx={{
-                display: "flex",
-                flexDirection: { lg: "row", md: "row", xs: "column" },
+        </FormControl>
+        <FormControl sx={inputStyle}>
+          <InputLabel htmlFor="address-input-box" required>
+            Address
+          </InputLabel>
+          <OutlinedInput
+            required
+            id="address-input-box"
+            label="Address"
+            onChange={handleForm("address")}
+          />
+        </FormControl>
+        <FormControl sx={inputStyle}>
+          <InputLabel id="state-select-label" required>
+            State
+          </InputLabel>
+          <Select
+            required
+            labelId="state-select-label"
+            id="state-select"
+            value={formData.state}
+            label="State"
+            onChange={handleForm("state")}
+          >
+            {stateList?.map((list, index) => {
+              return (
+                <MenuItem key={index} value={list.name}>
+                  {list.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormGroup
+          sx={{
+            display: "flex",
+            flexDirection: { lg: "row", md: "row", xs: "column" },
+          }}
+        >
+          <FormControl sx={{ flex: 1, ...inputStyle }}>
+            <Autocomplete
+              freeSolo
+              onChange={(event, newValue) => {
+                setFormData({ ...formData, city: newValue });
               }}
-            >
-              <FormControl sx={{ flex: 1, ...inputStyle }}>
-                <Autocomplete
-                  freeSolo
-                  onChange={(event, newValue) => {
-                    setFormData({ ...formData, city: newValue });
-                  }}
-                  onInputChange={(event, newInputValue) => {
-                    setFormData({ ...formData, city: newInputValue });
-                  }}
-                  disabled={formData.state === "" ? true : false}
-                  id="contribute-modal-city"
-                  options={newCityList.map((option) => option.label)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label={
-                        formData.state === ""
-                          ? "Please Select the state first"
-                          : "City"
-                      }
-                      InputProps={{
-                        ...params.InputProps,
-                        type: "search",
-                      }}
-                    />
-                  )}
-                />
-              </FormControl>
-              <FormControl
-                sx={{
-                  flex: 1,
-                  ...inputStyle,
-                  marginLeft: { lg: 1, md: 1, xs: 0 },
-                }}
-              >
-                <InputLabel htmlFor="zip-input-box" required>
-                  Zip
-                </InputLabel>
-                <OutlinedInput
-                  required
-                  id="zip-input-box"
-                  label="Zip"
-                  type="number"
-                  onChange={handleForm("zip")}
-                />
-              </FormControl>
-            </FormGroup>
-            <FormGroup
-              sx={{
-                display: "flex",
-                flexDirection: { lg: "row", md: "row", xs: "column" },
+              onInputChange={(event, newInputValue) => {
+                setFormData({ ...formData, city: newInputValue });
               }}
-            >
-              <FormControl sx={{ flex: 1, ...inputStyle }}>
-                <InputLabel htmlFor="mobile-input-box" required>
-                  Mobile
-                </InputLabel>
-                <OutlinedInput
-                  required
-                  id="mobile-input-box"
-                  label="Mobile"
-                  type="number"
-                  onChange={handleForm("mob")}
+              disabled={formData.state === "" ? true : false}
+              id="contribute-modal-city"
+              options={newCityList.map((option) => option.label)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label={
+                    formData.state === ""
+                      ? "Please Select the state first"
+                      : "City"
+                  }
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
                 />
-              </FormControl>
-              <FormControl
-                sx={{
-                  flex: 1,
-                  ...inputStyle,
-                  marginLeft: { lg: 1, md: 1, xs: 0 },
-                }}
-              >
-                <InputLabel htmlFor="email-input-box" required>
-                  Email
-                </InputLabel>
-                <OutlinedInput
-                  required
-                  id="email-input-box"
-                  label="Email"
-                  type="email"
-                  onChange={handleEmail}
-                />
-                {!isEmailValid && (
-                  <FormHelperText error id="email-error">
-                    Please enter valid email address
-                  </FormHelperText>
-                )}
-              </FormControl>
-            </FormGroup>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button
-              onClick={handleSubmitForm}
-              disabled={
-                formData.amount === "" ||
-                formData.amount === "other" ||
-                formData.name === "" ||
-                formData.address === "" ||
-                formData.city === "" ||
-                formData.state === "" ||
-                formData.zip === "" ||
-                formData.mob === "" ||
-                formData.email === "" ||
-                parseInt(formData.amount) === 0 ||
-                (formData.amount > 5000 && formData.panNumber === "") ||
-                !isEmailValid
-                  ? true
-                  : false
-              }
-            >
-              Submit
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    </div>
+              )}
+            />
+          </FormControl>
+          <FormControl
+            sx={{
+              flex: 1,
+              ...inputStyle,
+              marginLeft: { lg: 1, md: 1, xs: 0 },
+            }}
+          >
+            <InputLabel htmlFor="zip-input-box" required>
+              Zip
+            </InputLabel>
+            <OutlinedInput
+              required
+              id="zip-input-box"
+              label="Zip"
+              type="number"
+              onChange={handleForm("zip")}
+            />
+          </FormControl>
+        </FormGroup>
+        <FormGroup
+          sx={{
+            display: "flex",
+            flexDirection: { lg: "row", md: "row", xs: "column" },
+          }}
+        >
+          <FormControl sx={{ flex: 1, ...inputStyle }}>
+            <InputLabel htmlFor="mobile-input-box" required>
+              Mobile
+            </InputLabel>
+            <OutlinedInput
+              required
+              id="mobile-input-box"
+              label="Mobile"
+              type="number"
+              onChange={handleForm("mob")}
+            />
+          </FormControl>
+          <FormControl
+            sx={{
+              flex: 1,
+              ...inputStyle,
+              marginLeft: { lg: 1, md: 1, xs: 0 },
+            }}
+          >
+            <InputLabel htmlFor="email-input-box" required>
+              Email
+            </InputLabel>
+            <OutlinedInput
+              required
+              id="email-input-box"
+              label="Email"
+              type="email"
+              onChange={handleEmail}
+            />
+            {!isEmailValid && (
+              <FormHelperText error id="email-error">
+                Please enter valid email address
+              </FormHelperText>
+            )}
+          </FormControl>
+        </FormGroup>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button
+          onClick={handleSubmitForm}
+          disabled={
+            formData.amount === "" ||
+            formData.amount === "other" ||
+            formData.name === "" ||
+            formData.address === "" ||
+            formData.city === "" ||
+            formData.state === "" ||
+            formData.zip === "" ||
+            formData.mob === "" ||
+            formData.email === "" ||
+            parseInt(formData.amount) === 0 ||
+            (formData.amount > 5000 && formData.panNumber === "") ||
+            !isEmailValid
+              ? true
+              : false
+          }
+        >
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
