c8c38937ffb489720163b1bc249dcf42aeea2195
Anit Singh <anitsingh367@gmail.com>
2022-09-04 06:42:06 +0530

    <> img with avatar, + side gradients, - shit code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Events from "../components/Events/Events.react";
 import DrawerAppBar from "../components/Navbar/Navbar.react";
 import Testimonial from "../components/Testimonial/Testimonial.react";
 import Footer from "../components/Footer/Footer.react";
-import Volunteers from "../components/Volunteers/Volunteers.rect";
+import Volunteers from "../components/Volunteers/Volunteers.react";
 
 const Home = () => {
   return (
