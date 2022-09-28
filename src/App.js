49a55a0fcd97bc23faca11dd5b84eae80d8c830d
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-09-28 22:23:00 +0530

    about us structure done
    Basic styling remaining like image alignment and text font size & alignment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import "./App.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import DrawerAppBar from "./components/Navbar/Navbar.react";
+
 import ProjectsPage from "./components/Projects/ProjectsPage";
+
+import About from "./pages/About";
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/projects" element={<ProjectsPage />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
       <ToastContainer
         position="top-right"
