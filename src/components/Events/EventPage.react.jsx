061e7a65ede73cbe2516bc1f1dcf2821f92088f5
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-11-04 00:00:59 +0530

    Added Filter and Status features

--- src/components/Events/EventPage.react.jsx
+++ src/components/Events/EventPage.react.jsx
@@ -5,9 +5,10 @@ import {
   Container,
   Box,
   Pagination,
-  MenuItem,
-  Select,
+  ToggleButtonGroup,
+  ToggleButton,
 } from "@mui/material";
+import { FiberManualRecord as LiveDot } from "@mui/icons-material";
 import PropTypes from "prop-types";
 import ShareIcon from "@mui/icons-material/Share";
 
@@ -50,95 +51,88 @@ EventPages.defaultProps = {
     {
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqu39eyj7mkHZ2gnUmKmU9smZN8F3mI7xeC2DFXhTWwOSiL7JaliiMiC8NF3hZK-m1AD8&usqp=CAU",
-      heading: "Ongoing Projects",
+      heading: "This must be upcoming",
       description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium, voluptate harum iste sunt optio quo maxime repellat et mollitia.",
-      category: "Education",
-      status: "Ongoing",
+        "A New India Together fsdhfjsdhf sdkjfsdjkfhsdkj fhsdkjfhsdkjfhsdfkjsdhf ksjdfhsdkjfh sdkjfshdfkjsdfhkjsdhfksdjhfsd kjf fhdjshfksjd fhskdjfhskdjfh sdkjfhsdf kj sdfh",
+      status: "Upcoming",
+      date: {
+        start_date: "2023-09-06 19:00:00",
+        end_date: "2023-09-06 22:00:00",
+      },
     },
     {
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwB29eRCxE1_92bxreaZ5tsnqgQFgHScAFEA4nn4vpiMfLX-h1j-RhnZfCo9_IcFNx4E&usqp=CAU",
-      heading: "Ongoing Projects",
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium, voluptate harum iste sunt optio quo maxime repellat et mollitia.",
-      category: "Education",
-      status: "Ongoing",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1550330562-b055aa030d73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      heading: "Future Projects",
-      description:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus laudantium, voluptate harum iste sunt optio quo maxime repellat et mollitia.",
-      category: "Medical",
-      status: "Future",
-    },
-    {
-      image: "",
-      heading: "Accomplished 4",
-      description:
-        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      category: "Medical",
-      status: "Accomplished",
-    },
-    {
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqu39eyj7mkHZ2gnUmKmU9smZN8F3mI7xeC2DFXhTWwOSiL7JaliiMiC8NF3hZK-m1AD8&usqp=CAU",
-      heading: "Future 1",
-      description: "Description 1",
-      category: "Medical",
-      status: "Future",
-    },
-    {
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwB29eRCxE1_92bxreaZ5tsnqgQFgHScAFEA4nn4vpiMfLX-h1j-RhnZfCo9_IcFNx4E&usqp=CAU",
-      heading: "Future 2",
+      heading: "This must be finished",
       description: "Description 2",
-      category: "Education",
-      status: "Future",
+      status: "Finished",
+      date: {
+        start_date: "2022-09-05 20:00:00",
+        end_date: "2022-09-05 22:00:00",
+      },
     },
     {
       image:
         "https://images.unsplash.com/photo-1550330562-b055aa030d73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      heading: "Future 3",
+      heading: "This must be live",
       description: "Description 3",
-      category: "Medical",
-      status: "Future",
+      status: "Live",
+      date: {
+        start_date: "2022-09-05 19:00:00",
+        end_date: "2022-09-09 03:06:00",
+      },
     },
     {
       image: "",
-      heading: "Ongoing 4",
+      heading: "This must be upcoming",
       description:
         "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
-      category: "Education",
-      status: "Ongoing",
+      status: "Upcoming",
+      date: {
+        start_date: "2022-09-11 19:00:00",
+        end_date: "2022-09-12 03:06:00",
+      },
+    },
+    {
+      image: "",
+      heading: "This must be upcoming",
+      description:
+        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
+      status: "Upcoming",
+      date: {
+        start_date: "2022-09-11 19:00:00",
+        end_date: "2022-09-12 03:06:00",
+      },
     },
   ],
 };
 
 export default function EventPages(props) {
+  // Handle status
   const [status, setStatus] = useState("All");
-  //Handle Category filter
-
-  //Handle Status filter
-  const handleChangeFilter = (event) => {
-    setStatus(event.target.value);
-  };
-
-  const [eventFilter, setEventFilter] = useState(props.content);
+  const [eventFilter, setProjectFilter] = useState(props.content);
 
   useEffect(() => {
     const filteredData = props.content.filter((item) => {
       let itemStatus = item.status;
-      return status === "All"
-        ? item
-        : status !== "All"
-        ? itemStatus === status
-        : status === "All";
+      return status === "All" ? item : itemStatus === status;
     });
-    setEventFilter(filteredData);
+    setProjectFilter(filteredData);
   }, [status, props.content]);
+
+  const handleChangeToggle = (event) => {
+    if (event.target.value !== null) {
+      setStatus(event.target.value);
+    }
+  };
+
+  // Status Color
+  const statusColor = {
+    Live: "#ED0000",
+    Upcoming: "#388E3C",
+    Finished: "#999999",
+  };
+
   return (
     <Container
       maxWidth={false}
@@ -182,12 +176,23 @@ export default function EventPages(props) {
             alignitems: "center",
           }}
         >
-          <Select value={status} onChange={handleChangeFilter}>
-            <MenuItem value="All">All</MenuItem>
-            <MenuItem value="Ongoing">Ongoning</MenuItem>
-            <MenuItem value="Accomplished">Accomplished</MenuItem>
-            <MenuItem value="Future">Future</MenuItem>
-          </Select>
+          <ToggleButtonGroup
+            aria-label="text button group"
+            size="large"
+            color="primary"
+            exclusive
+            value={status}
+            onChange={handleChangeToggle}
+            sx={{
+              display: "flex",
+              justifyContent: { sm: "center", xs: "center" },
+            }}
+          >
+            <ToggleButton value="All">All</ToggleButton>
+            <ToggleButton value="Live">Live</ToggleButton>
+            <ToggleButton value="Upcoming">Upcoming</ToggleButton>
+            <ToggleButton value="Finished">Finished</ToggleButton>
+          </ToggleButtonGroup>
         </Box>
       </Container>
       <Container
@@ -213,7 +218,11 @@ export default function EventPages(props) {
                   image: items.image,
                   heading: items.heading,
                   description: items.description,
-                  chipTemplate: { chipText: items.category },
+                  chipTemplate: {
+                    icon: items.status === "Live" ? LiveDot : null,
+                    chipText: items.status,
+                    textColor: statusColor[items.status],
+                  },
                   primaryBtn: {
                     btnText: "View Details",
                   },
