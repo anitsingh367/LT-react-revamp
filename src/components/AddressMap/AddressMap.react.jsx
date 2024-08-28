20554479a34800ea68f49540d163b5670632c57a
atharvamulgund <mulgundatharva@gmail.com>
2024-08-28 21:16:35 +0530

    feat: added mapUrl in addressUrl component

--- src/components/AddressMap/AddressMap.react.jsx
+++ src/components/AddressMap/AddressMap.react.jsx
@@ -1,6 +1,14 @@
 import { Box } from "@mui/material";
 import React from "react";
-export default function AddressMap() {
+import PropTypes from "prop-types";
+import { getMapUrl } from "../../utilis/get-map-url";
+
+AddressMap.propTypes = {
+  mapUrl: PropTypes.string
+}
+
+export default function AddressMap(props) {
+  const src = getMapUrl(props?.mapUrl)
   return (
     <Box
       sx={{
@@ -9,7 +17,7 @@ export default function AddressMap() {
       }}
     >
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
+        src={src}
         frameBorder="0"
         height="100%"
         width="100%"
